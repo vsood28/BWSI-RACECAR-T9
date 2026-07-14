9a7722959c64b9ffb15f8e248f37d61f290deeda
Viir Sood <viirnsood@gmail.com>
2026-07-14 15:59:12 -0400

    depression

--- racecar-student/QUEST_LOG/LineFollower.py
+++ racecar-student/QUEST_LOG/LineFollower.py
@@ -2,21 +2,33 @@ import sys
 import cv2 as cv
 import numpy as np
 
-sys.path.insert(1, '../../../library')
+sys.path.insert(1, '../../library')
 import racecar_core
 import racecar_utils as rc_utils
-import time
 import importlib
 import untitled
+
 
 rc = racecar_core.create_racecar()
 
 global maxc # max contour area of blue mask
 maxc = None
-MIN_CONTOUR_AREA = 9000 # tune
+MIN_CONTOUR_AREA = 4000 # tune
 
 # check the crop and hsv values
 CROP = ((240, 0), (rc.camera.get_height(), rc.camera.get_width()))
+
+global error
+error = 0.0
+
+global lastError
+lastError = error
+
+global filteredError
+filteredError = 0.0
+
+global lastFilteredError
+lastFilteredError = 0.0
 
 speed = 0.0
 angle = 0.0
@@ -40,9 +52,9 @@ def update_contour():
 
     image = rc_utils.crop(image, CROP[0], CROP[1])
     hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
-
+    hsv = cv.GaussianBlur(hsv, (5,5), 0)
     blue_mask = cv.inRange(hsv, untitled.BLUE[0], untitled.BLUE[1])
- 
+    blue_mask = cv.morphologyEx(blue_mask, cv.MORPH_OPEN, np.ones((5,5), np.uint8))
     blue_contours, _ = cv.findContours(blue_mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     
     # precondition: called
@@ -68,49 +80,60 @@ def update_contour():
         contour_area = 0
         maxc = None
     cv.drawContours(image, blue_contours, -1, (255,0,0), 3)   
-    rc.display.show_color_image(image)
-    
+    #rc.display.show_color_image(image)
 
-def start(): # initializes values
+
+def start():
     global speed
     global angle
     global contour_center_filtered
+    global filteredError
+    global lastFilteredError
     speed = 0
     angle = 0
     contour_center_filtered = None
+    filteredError = 0.0
+    lastFilteredError = 0.0
+
     rc.drive.set_speed_angle(speed, angle)
     rc.set_update_slow_time(0.5)
     rc.drive.set_max_speed(1)
 
 
-global error
-error = 0.0
-
-def update(): 
-    importlib.reload(untitled)
-    # calls update_contour and sets the angle of the car depending on where the line is
-    # also changes the angle of the car so it makes less jerky turns and is smooth
+def update():
     global speed
     global angle
     global contour_center_filtered
     global last_angle
     global maxc
-    global error 
+    global error
     global contour_center
-
+    global lastError
+    global filteredError
+    global lastFilteredError
+    importlib.reload(untitled)
     update_contour()
 
     if contour_center is not None:
         contour_center_filtered = contour_center[1]
-        error = (contour_center_filtered - untitled.CAMERA_OFFSET) - (rc.camera.get_width() // 2) # tune offset
-        angle = (untitled.KP * error) - untitled.ANGLE_OFFSET
+        error = (contour_center_filtered - untitled.CAMERA_OFFSET) - (rc.camera.get_width() // 2)
+        filteredError = 0.8 * filteredError + 0.2 * error
+
+        angle = (untitled.KP * filteredError) - untitled.ANGLE_OFFSET
+
+        print("P")
+        print(untitled.KP * filteredError)
         angle = rc_utils.clamp(angle, -1, 1)
     else:
-        angle = last_angle 
-    rc.drive.set_speed_angle(1, angle)
+        angle = last_angle
+
+    lastFilteredError = filteredError
+    lastError = error
+    rc.drive.set_speed_angle(0.7, angle)
     rc.telemetry.declare_variables("Speed", "Angle", "Error")
     rc.telemetry.record(speed, angle, error)
     last_angle = angle
+
 
 def update_slow():
     global maxc
