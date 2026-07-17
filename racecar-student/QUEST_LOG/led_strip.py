747af0062f376a0831c08674c2a7a3618f6fd086
Yevhenii22 <129606262+Yevhenii22@users.noreply.github.com>
2026-07-17 10:29:57 -0400

    Update led_strip.py

--- racecar-student/QUEST_LOG/led_strip.py
+++ racecar-student/QUEST_LOG/led_strip.py
@@ -1,24 +1,28 @@
-from matplotlib.pyplot import rc
-
-import led_strip
 import sys
-import numpy as np
 
 sys.path.insert(1, '../../library')
 import racecar_core
-import racecar_utils as rc_utils
-import time
 
-def start(): # initializes values
-   n  = rc.led.get_num_leds()
-   for i in range(n):
-       rc.led.set_color(i, (0, 0, 0))
+rc = racecar_core.create_racecar()
+
+ORANGE = (50, 255, 0)
 
 
-def update(): 
-    rc.led.set_color(0, (255, 100, 0))
+def start():
+   
+
+        n = rc.led.get_num_pixels()
+        print("LED strip found: {} pixels".format(n))
+
+        rc.led.set_pixel(25, (10, 0, 0))
+
+        print("Pixels now:", rc.led.get_pixels()[:4])
+
+    
+def update():
+    pass
 
 
 if __name__ == "__main__":
-    rc.set_start_update(start, update, update_slow)
+    rc.set_start_update(start, update)
     rc.go()
