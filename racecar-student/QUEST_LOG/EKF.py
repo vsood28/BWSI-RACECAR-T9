625919ed023d105c3dc036e8c7e544dd67ee56dd
tacocat1234 <sixianzheng09@gmail.com>
2026-07-20 11:24:18 -0400

    finsihed ekf?

--- racecar-student/QUEST_LOG/EKF.py
+++ racecar-student/QUEST_LOG/EKF.py
@@ -689,6 +689,21 @@ def process_noise_jacobian(state_estimate, control_input, delta_t, **kwargs): #x
         [0, delta_t]
     ])
 
+def state_model(state_estimate, control_input, delta_t, **kwargs):
+    x, y, theta, sigma = state_estimate #gives effect of process noise. since noise enters through controls exclusively in this model, control jacobian is not needed
+    v, omega = control_input
+    L = kwargs.get('wheelbase', WHEELBASE)
+
+    return np.array([
+        x + v * math.sin(theta) * delta_t,
+        y + v * math.cos(theta) * delta_t,
+        theta + v/L * math.tan(sigma) * delta_t,
+        sigma + omega * delta_t
+    ])
+
+def measurement_model(state_estimate, delta_time, **kwargs): #maps state to predicted measurement
+    return state_estimate
+
 class ExtendedKalmanFilter:
     def __init__(self,
                  initial_state_estimate,
@@ -697,7 +712,9 @@ class ExtendedKalmanFilter:
                  measurement_noise_covariance,
                  state_transistion_jacobian,
                  measurement_jacobian,
-                 process_noise_jacobian): #last three are funcs
+                 process_noise_jacobian,
+                 state_model,
+                 measurement_model): #last 5 are funcs
 
         self.state_estimate = initial_state_estimate
         self.state_estimation_covariance = initial_state_covariance
@@ -708,24 +725,40 @@ class ExtendedKalmanFilter:
         self.measurement_jacobian = measurement_jacobian
         self.process_noise_jacobian = process_noise_jacobian
 
+        self.state_model = state_model
+        self.measurement_model = measurement_model
+
         self.kalman_gain = None
 
         self.change_in_predicted_state = np.zeros(3) #accumulate imu steps, while waiting for lidar input
 
-    def predict_state(self, control_input, imu_transform): #imu based, imu transform (state estimation function) evaluated in odom section
-        self.change_in_predicted_state += imu_transform
-        F = self.state_transistion_jacobian()
+    def predict_state(self, control_input, delta_t, **kwargs): #encoder update
+        self.change_in_predicted_state += self.state_model(self.state_estimate, control_input) #update predicted state
 
-    def update_kalman_gain(self, control_input, delta_t, **kwargs):
-        H = self.measurement_jacobian(self.state_estimate, control_input, delta_t, **kwargs) #get jacobian
+        F = self.state_transistion_jacobian(self.state_estimate, control_input, delta_t, **kwargs)
+        P = self.state_estimation_covariance
+        Q = self.process_noise_covariance
+        G = self.process_noise_jacobian(self.state_estimate, control_input, delta_t, **kwargs)
+
+        self.state_estimation_covariance = F @ P @F.T + G @ Q @ G.T
+        
+    def update_state(self, true_measurement, delta_t, **kwargs): #measurement step, compares predicted measurement based on state to true measurement, finds difference, applies correction
+        predicted_measurement = self.measurement_model(self.state_estimate, delta_t, **kwargs)
+        innovation = true_measurement - predicted_measurement #difference
+
+        H = self.measurement_jacobian(self.state_estimate, delta_t, **kwargs) #get jacobian
         P = self.state_estimation_covariance
         R = self.measurement_noise_covariance
+        I = np.identity(len(self.state_estimate))
 
         S = H @ P @ H.T + R #innovation covariance: basically, hpht is uncertainty of state translated into uncertainty of measurement, R, is uncertainty due to sensor noise, sum is "measurement error" (not exactly, close enough)
         self.kalman_gain = P @ H.T @ np.linalg.inv(S) # pht translates from "how wrong is my measurement" (innovation, or S) to how states are affeced by how wrong the measruement is
 
-    def update_state(self, control_input, lidar_transform):
-        pass #lidar based
+        self.state_estimate += self.kalman_gain * innovation
+
+        L = (I - self.kalman_gain @ innovation)
+
+        self.state_estimation_covariance = L @ self.state_estimation_covariance @ L.T + self.kalman_gain @ R @ self.kalman_gain.T #joseph form
 
 class KalmanFilter: #1d, for processign IMU and encoder datas
     def __init__():
