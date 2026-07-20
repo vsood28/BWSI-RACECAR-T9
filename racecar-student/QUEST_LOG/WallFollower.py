7db09ed3a82c0deae9f4d05bf491bcc1c6b91b44
Viir Sood <viirnsood@gmail.com>
2026-07-20 16:23:56 -0400

    ICP changes

--- racecar-student/QUEST_LOG/WallFollower.py
+++ racecar-student/QUEST_LOG/WallFollower.py
@@ -54,7 +54,7 @@ def update():
     angle = WFC.KP * error + WFC.KD * (error - lastError) / dt
     print(f"Proportional term: {WFC.KP * error},   Derivative term: {WFC.KD * (error - lastError) / dt}")
    
-
+    
     elapsed = time.time() - start_time
     log_writer.writerow([elapsed, error, angle, error * WFC.KP, ((error - lastError) / dt) * WFC.KD])
     lastError = error
