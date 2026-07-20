9f9f208299d036ae79f18c365375b7d6ec204202
aTree <thefogiscoming142@gmail.com>
2026-07-20 17:48:17 -0400

    finishing IMU node

--- ros2_test/filters.py
+++ ros2_test/filters.py
@@ -2,23 +2,27 @@ import math
 import time
 
 class ComplementaryFilter:
-    def __init__(self, init_alpha):
+    def __init__(self, init_alpha, init_roll, init_pitch, init_yaw):
         self.alpha = init_alpha
-        self.roll = 0.0
-        self.pitch = 0.0
-        self.yaw = 0.0
-        self.dt = 0.0 # None ?
+        self.roll = init_roll
+        self.pitch = init_pitch
+        self.yaw = init_yaw
+        self.theta = 0.0
+        self.dt = 0.0
 
-    def update(self, ax, ay, az, gx, gy, gz): # accel and gyroscope (angular velo) values
+    def update(self, ax, ay, az, wx, wy, wz, mz, mx): # accel, gyroscope (angular velocity), and magnetometer values
         self.dt = time.time() - self.dt
 
         accel_roll = math.atan2(ay, az) 
         accel_pitch = math.atan2(-ax, math.sqrt(ay**2 + az**2))
+        theta_mag = math.atan2(mz, mx) # axis convention could be off
         
-        self.roll = self.alpha * (self.roll + gx * self.dt) + ((1 - self.alpha) * accel_roll)
-        self.pitch = self.alpha * (self.pitch + gy * self.dt) + ((1 - self.alpha) * accel_pitch)
-        self.yaw = self.alpha * (self.yaw + gz * self.dt) # i think the second term is removed because accelerometer would give 0?
+        self.roll = self.alpha * (self.roll + wx * self.dt) + ((1 - self.alpha) * accel_roll)
+        self.pitch = self.alpha * (self.pitch + wy * self.dt) + ((1 - self.alpha) * accel_pitch)
+        self.yaw = self.alpha * (self.yaw + wz * self.dt) # i think the second term is removed because accelerometer would give 0?
+        self.theta = self.alpha * (self.theta + wy * self.dt) + ((1 - self.alpha) * theta_mag)
         
+        return self.roll, self.pitch, self.yaw, self.theta
 
 class KalmanFilter:
     # i hate this system
@@ -37,7 +41,7 @@ class KalmanFilter:
 
     # ordering:
     # 1. add process noise 
-    # 2. calculate kalmain gain
+    # 2. calculate kalman gain
     # 3. estimate x n+1
     # 4. update est
     def update(self, nvalue):
@@ -45,3 +49,4 @@ class KalmanFilter:
         self.kg = self.covar_est / (self.covar_est + self.covar_mea)
         self.n = self.n + (self.kg * (nvalue - self.n))
         self.covar_est = (1 - self.kg) * self.covar_est
+        return self.n
--- ros2_test/imu.py
+++ ros2_test/imu.py
@@ -4,7 +4,7 @@ import rclpy
 from rclpy.node import Node  
 
 from geometry_msgs.msg import Pose2D, Vector3
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import Imu, MagneticField # does magneticfield work like this ?
 from std_msgs.msg import Float32
 
 import filters
@@ -12,12 +12,14 @@ import math
 import time
 
 ### Classes ###
+# self = global
 
-# what is the parameter of this class
+# what is this parameter
 class ImuNode(Node):
     def __init__(self):
         super().__init__('imu_publisher_node')
         
+        # is this right
         self.declare_parameter('pubilsh_rate_hz', 60.0)
 
         # create_publisher parameters: msg type (Class), topic (str), quality (10)
@@ -25,53 +27,115 @@ class ImuNode(Node):
         self.__velocity_pub = self.create_publisher(Float32, '/velocity', 10)
         self.__pose_est_pub = self.create_publisher(Pose2D, '/pose_estimate', 10)
 
+        # messages to publish
         self.__attitude_message = Vector3()
         self.__velocity_message = Float32()
-        self.__pose_est_message = Pose() # ?
+        self.__pose_est_message = Pose2D()
 
-        self.kf1_velocity = filters.KalmanFilter()
-        self.kf2 = filters.KalmanFilter()
-        self.kf3 = filters.KalmanFilter()
+        # Filters and extra useful variables ??     + usage on right side
+        self.kf1_velocity = filters.KalmanFilter() # linear velocity
+        self.kf2_posx = filters.KalmanFilter() # position (x)
+        self.kf3_posz = filters.KalmanFilter() # position (y)
         
-        self.comp1 = filters.ComplementaryFilter()
+        self.compf1_att = filters.ComplementaryFilter() # attitude
+        self.compf2_theta = filters.ComplementaryFilter() # theta
 
-        self.dt = 0.0
+        # do these get initialized here or in callback
+        self.dt = 0.0 # is this initialized with 0 because it should be an integer
 
-        # im confused
+        self.velocity_x = 0.0 
+        self.velocity_z = 0.0
+
+        self.roll = 0.0
+        self.pitch = 0.0
+        self.yaw = 0.0  
+
+        self.position_x = 0.0
+        self.position_z = 0.0
+
+        self.mx = 0.0
+        self.my = 0.0
+        self.mz = 0.0
+
+        # create_subscription parameters: msg_type, topic, callback, qos_profile
         self.create_subscription(Imu, '/imu/fused', self.imu_fused_callback, 10)
+        self.create_subscription(MagneticField, '/mag', self.mag_callback, 10)
 
 
     def imu_fused_callback(self, data): # called every time something is published
-        # self = global
-        self.dt = time.time() - self.dt
+        self.dt = time.time() - self.dt # dt for all integration
+        
+        ########################## LINEAR VELOCITY ##########################
 
-        # linear velocity:
+        # integrating acceleration values
         self.velocity_x = self.velocity_x + data.linear_acceleration.x * self.dt
-        # y should be equal to 0 - ignorable?
+        # self.velocity_y = self.velocity_y + data.linear_acceleration.y * self.dt # y should technically be equal to zero - maybe ignorable?
         self.velocity_z = self.velocity_z + data.linear_acceleration.z * self.dt
 
+        # combines velocity into a scalar 
         self.velocity_scalar = self.velocity_x ** 2 + self.velocity_z ** 2
-        
-        self.velocity_final
 
-        self.__velocity_message.data 
+        # passing velocity into a kalman filter
+        self.kf1_velocity.__init__(self, 0, 0) # is self correct here?
+        self.__velocity_message.data = self.kf1_velocity.update(self, self.velocity_scalar)
+
+        # publishing final scalar velocity
         self.__velocity_pub.publish(self.__velocity_message)
 
-        # attitude:
-        self.
+        ############################## ATTITUDE ###############################
 
-        # pose: x,y = x,z
+        # integrating angular velocity values
+        self.roll = self.roll + data.angular_velocity.x * self.dt
+        self.pitch = self.pitch + data.angular_velocity.y * self.dt
+        self.yaw = self.yaw + data.angular_velocity.z * self.dt
 
-        data.angular_velocity.x
-        data.angular_velocity.y
-        data.angular_velocity.z
+        # passing values into a complementary filter 
+        self.compf1_att.__init__(self, 0.95, self.roll, self.pitch, self.yaw) # alpha value: trust to put into gyroscope
+        at_x, at_y, at_z, _ = self.compf1_att.update(self, 
+                           data.linear_acceleration.x, data.linear_acceleration.y, data.linear_acceleration.z, 
+                           data.angular_velocity.x, data.angular_velocity.y, data.angular_velocity.z,
+                           None, None)
+    
+        # taking the values out of complementary filter and assigning them to the message data
+        self.__attitude_message.x = at_x
+        self.__attitude_message.y = at_y
+        self.__attitude_message.z = at_z
 
-
-
-        self.__attitude_message.x = 0
-        self.__attitude_message.y = 0
-        self.__attitude_message.z = 0
+        # publishing final attitude components
         self.__attitude_pub.publish(self.__attitude_message)
+
+        ############################### POSE ####################################
+        # x,y = x,z because y has the gravity acceleration in it for some reason
+        
+        # integrating linear velocity values
+        self.position_x = self.position_x + self.velocity_x * self.dt
+        self.position_z = self.position_z + self.velocity_z * self.dt
+
+        # passing x and z into kalman filters
+        self.kf2_posx.__init__(self, 0, 0)
+        self.kf3_posz.__init__(self, 0, 0)
+
+        # passing theta into complementary filter
+        self.compf2_theta.__init__(self, 0.95, None, None, None) # alpha value: trust to put into gyroscope
+        _, _, _, final_theta = self.compf1_att.update(self, 
+                           None, None, None,
+                           None, None, None,
+                           self.mz, self.mx)
+
+        # assigning various values to message data
+        self.__pose_est_message.data.x = self.kf2_velocity.update(self, self.position_x)
+        self.__pose_est_message.data.y = self.kf3_velocity.update(self, self.position_z)
+        self.__pose_est_message.data.theta = final_theta
+
+        # publishing final 2d pose values
+        self.__pose_est_pub.publish(self.__pose_est_message)
+
+
+    def mag_callback(self, data):
+        self.mx = data.magnetic_field.x 
+        # self.my = data.magnetic_field.y 
+        self.mz = data.magnetic_field.z 
+
 
 def main():
     rclpy.init(args=None)
