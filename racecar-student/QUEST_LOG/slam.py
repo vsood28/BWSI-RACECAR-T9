fa9e887659c5548b89dff3e2bbdb9162b87b473b
tacocat1234 <sixianzheng09@gmail.com>
2026-07-22 15:11:09 -0400

    ftg gab, mod slam

--- racecar-student/QUEST_LOG/slam.py
+++ racecar-student/QUEST_LOG/slam.py
@@ -1,7 +1,22 @@
-class SLAM:
-    def __init__(self, ekf, occupancy_grid):
+import rclpy as ros2
+
+class slam(Node):
+    def __init__(self, ekf, occupancy_grid, models, jacobians, sys_params):
         self.ekf = ekf
         self.occupancy_grid = occupancy_grid
+
+        s, m = models.values()
+
+        self.state_model = s
+        self.measurement_model = m
+
+        s, m, p = jacobians.values()
+
+        self.state_trans_jacobian = s
+        self.measurement_jacobian = m
+        self.process_noise_jacobian = p
+
+        self.sys_params = sys_params
 
     def get_state_estimate(self):
         return self.ekf.state_estimate
@@ -11,3 +26,7 @@ class SLAM:
     
     def get_map_scale(self):
         return self.occupancy_grid.resolution
+    
+    def estimate_callback(self, control_input, delta_t):
+        self.ekf.predict_state(control_input, delta_t, **self.sys_params)
+        
