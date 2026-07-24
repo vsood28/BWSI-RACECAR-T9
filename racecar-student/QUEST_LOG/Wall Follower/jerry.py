b35b07ed7625dfbe6f34ec3ea68713496ab140ad
aTree <thefogiscoming142@gmail.com>
2026-07-24 10:41:07 -0400

    Create jerry.py

---
+++ racecar-student/QUEST_LOG/Wall Follower/jerry.py
@@ -0,0 +1,67 @@
+MAX_SPEED = 1.0
+
+angle = 0.0
+speed = MAX_SPEED
+last_error = 0.0
+
+kP = 1.1
+kD = 0.02
+
+import sys
+import time
+
+import numpy as np
+
+sys.path.insert(0, "../../library")
+import racecar_core
+import racecar_utils as rc_utils
+
+rc = racecar_core.create_racecar(isSimulation=False)
+time.sleep(2)
+
+def findMaxW(scan, minA, maxA, window):
+    length = scan.shape[0]
+
+    originA = minA
+    minA %= 360
+    maxA %= 360
+
+    start = int(minA * length / 360)
+    end = int(maxA * length / 360)
+
+    if start > end:
+        sScan = np.append(scan[start:], scan[:end + 1])
+    else:
+        sScan = scan[start:end + 1]
+
+    wSize = int(window * length / 360)
+    sums = np.convolve(sScan, np.ones(wSize), "valid")
+    center = int(sums.argmax()) + wSize // 2
+
+    return originA + center * 360 / length
+
+def start():
+    rc.drive.stop()
+    rc.set_update_slow_time(0.1)
+    rc.drive.set_max_speed(MAX_SPEED)
+
+
+def update():
+    global speed, angle, last_error
+
+    scan = rc.lidar.get_samples()
+
+    raw_gap = findMaxW(scan, -90, 90, 30)
+
+    error = raw_gap / 180
+    dt = rc.get_delta_time()
+    d_error = (error - last_error) / dt if dt > 0 else 0.0
+    last_error = error
+    angle = float(np.clip(error * kP + d_error * kD, -1.0, 1.0))
+    speed = MAX_SPEED
+
+    rc.drive.set_speed_angle(speed, rc_utils.clamp(angle + 0.04, -1.0, 1.0))
+    print(raw_gap)
+
+rc.set_start_update(start, update)
+rc.go()
